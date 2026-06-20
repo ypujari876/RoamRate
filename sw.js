7b4b8d6f2218b331c31f477dd022de0e0518ccbf
ypujari876 <pujari.yashita@gmail.com>
2026-06-20 23:45:43 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'roamrate-v1';
+const ASSETS = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
